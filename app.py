ae41c88b61344ad5f7cfe1135b27c7f9344ba6df
kavinraju <rajukavin25@gmail.com>
2020-05-03 12:09:23 +0530

    Implemented migration concepts using flask_migrate, also well dcoumented

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template, url_for, request, redirect, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import sys
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:skr123@localhost:5432/todoapp'
 
+# #1stThing 
+#  Initialize Flask-Migrate -  linking it to the Flask app models and database, 
+# link to command line scripts for running migrations, set up folders to store migrations
+migrate = Migrate(app,db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+# db.create_all() - we need not have this here as we want the migrate versions to take care of create a table...
+# #2ndThing - Run python -m flask db init so that it creates migration folder + necessary files
+# #3rdThing - Run python -m flask db migrate to create a migrate script under versions folder. If any changes to be done can be done there.
+# #4thThing - Run python -m flask db updrade to roll-out the DB upgrade
+
 
 @app.route('/todos/create', methods=['POST'])
 def todo_create():
